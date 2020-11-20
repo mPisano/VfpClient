dce1a8b01995b110a8b838f82b8791fd4795de7b
mPisano <myemailid@domain.com>
2020-11-20 14:07:25 -0500

    Support vs2017

---
+++ Source/VfpClient/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.5.15.0")]
+[assembly: AssemblyFileVersion("0.5.15.0")]
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("VfpClient")]
+[assembly: AssemblyTrademark("")]
